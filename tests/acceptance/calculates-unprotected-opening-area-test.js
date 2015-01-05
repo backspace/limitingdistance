8897a3295490e335a50fa9358a6dd76192a4e418
Buck Doyle <b@chromatin.ca>
2015-01-05 11:11:35 -0500

    Introduce unit conversion layer in face component

--- tests/acceptance/calculates-unprotected-opening-area-test.js
+++ tests/acceptance/calculates-unprotected-opening-area-test.js
@@ -59,3 +59,28 @@ test('displays the correct calculation result for the second group', function() 
     ok(unprotectedOpeningArea === '16.0');
   });
 });
+
+test('displays the correct imperial calculation result', function() {
+  visit('/');
+
+  // FIXME currently required to activate observers
+  andThen(function() {
+    window.loaded();
+  });
+
+  click('label:contains(Imperial) input');
+  click('label:contains(F3) input');
+  click('label:contains(Unsprinklered) input');
+
+  fillIn('.face-1 input[name=height]', 30);
+  fillIn('.face-1 input[name=width]', 30);
+  fillIn('.face-1 input[name=distance]', 9);
+
+  andThen(function() {
+    // FIXME currently required to trigger area calculation
+    find('.face-1 input[name=width]').trigger('keyup');
+    // return pauseTest();
+    var unprotectedOpeningArea = find('.face-1 input[name=area]').val();
+    ok(unprotectedOpeningArea === '43.0');
+  });
+});
