a297d0e129cec15a0d087bf2d11137d8d345ea3e
Buck Doyle <b@chromatin.ca>
2015-01-05 10:40:02 -0500

    Delegate project-specific attributes to Project

--- tests/acceptance/calculates-unprotected-opening-area-test.js
+++ tests/acceptance/calculates-unprotected-opening-area-test.js
@@ -12,7 +12,7 @@ module('Acceptance: Calculates unprotected opening area', {
   }
 });
 
-test('displays the correct calculation result', function() {
+test('displays the correct calculation result for the first group', function() {
   visit('/');
 
   // FIXME currently required to activate observers
@@ -33,5 +33,29 @@ test('displays the correct calculation result', function() {
     find('.face-1 input[name=width]').trigger('keyup');
     var unprotectedOpeningArea = find('.face-1 input[name=area]').val();
     ok(unprotectedOpeningArea === '7.0');
+  });
+});
+
+test('displays the correct calculation result for the second group', function() {
+  visit('/');
+
+  // FIXME currently required to activate observers
+  andThen(function() {
+    window.loaded();
+  });
+
+  click('label:contains(Metric) input');
+  click('label:contains(F1) input');
+  click('label:contains(Unsprinklered) input');
+
+  fillIn('.face-1 input[name=height]', 3);
+  fillIn('.face-1 input[name=width]', 10);
+  fillIn('.face-1 input[name=distance]', 3.5);
+
+  andThen(function() {
+    // FIXME currently required to trigger area calculation
+    find('.face-1 input[name=width]').trigger('keyup');
+    var unprotectedOpeningArea = find('.face-1 input[name=area]').val();
+    ok(unprotectedOpeningArea === '16.0');
   });
 });
