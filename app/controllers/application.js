1c53d0e486a40673941e6e571a15f6924ee4d013
Buck Doyle <b@chromatin.ca>
2015-01-04 20:11:10 -0500

    Introduce Project model

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,13 +1,13 @@
 import Ember from 'ember';
 
-import Face from '../models/face';
+import Project from '../models/project';
 
 export default Ember.Controller.extend({
   faceCount: function() {
     return `faces-${this.get('faces.length')}`;
   }.property('faces'),
 
-  faces: function() {
-    return [Face.create({index: 1}), Face.create({index: 2}), Face.create({index: 3}), Face.create({index: 4})];
-  }.property()
+  faces: Ember.computed.alias('project.faces'),
+
+  project: Project.create()
 });
