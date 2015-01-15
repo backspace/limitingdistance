647051d4e1a68fec96d30ff3ef9e6319fd0b22d7
Buck Doyle <b@chromatin.ca>
2015-01-14 22:37:58 -0500

    Move area-to-distance to its own file

--- tests/acceptance/calculates-unprotected-opening-area-test.js
+++ tests/acceptance/calculates-unprotected-opening-area-test.js
@@ -44,30 +44,6 @@ test('displays the correct calculation result for the first group', function() {
   });
 });
 
-test('supports entering the unprotected area to calculate the limiting distance', function() {
-  visit('/');
-
-  // FIXME currently required to activate observers
-  andThen(function() {
-    window.loaded();
-  });
-
-  click('label:contains(Metric) input');
-  click('label:contains(F3) input');
-  click('label:contains(Unsprinklered) input');
-
-  fillIn('.face-1 input[name=height]', 5);
-  fillIn('.face-1 input[name=width]', 5);
-  fillIn('.face-1 input[name=area]', 7);
-
-  andThen(function() {
-    // FIXME currently required to trigger area calculation
-    find('.face-1 input[name=area]').trigger('keyup');
-    var limitingDistance = find('.face-1 input[name=distance]').val();
-    equal(limitingDistance, '1.2', 'should have limiting distance of 1.2');
-  });
-});
-
 test('displays the correct calculation result for the second group', function() {
   visit('/');
 
