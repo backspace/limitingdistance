44c01f0029f3191582a8d0326d7dca78c70b327a
Buck Doyle <b@chromatin.ca>
2015-01-14 22:37:58 -0500

    Move area-to-distance to its own file

---
+++ tests/acceptance/calculates-limiting-distance-test.js
@@ -0,0 +1,37 @@
+import Ember from 'ember';
+import startApp from '../helpers/start-app';
+
+var application;
+
+module('Acceptance: Calculates limiting distance', {
+  setup: function() {
+    application = startApp();
+  },
+  teardown: function() {
+    Ember.run(application, 'destroy');
+  }
+});
+
+test('supports entering the unprotected area to calculate the limiting distance', function() {
+  visit('/');
+
+  // FIXME currently required to activate observers
+  andThen(function() {
+    window.loaded();
+  });
+
+  click('label:contains(Metric) input');
+  click('label:contains(F3) input');
+  click('label:contains(Unsprinklered) input');
+
+  fillIn('.face-1 input[name=height]', 5);
+  fillIn('.face-1 input[name=width]', 5);
+  fillIn('.face-1 input[name=area]', 7);
+
+  andThen(function() {
+    // FIXME currently required to trigger area calculation
+    find('.face-1 input[name=area]').trigger('keyup');
+    var limitingDistance = find('.face-1 input[name=distance]').val();
+    equal(limitingDistance, '1.2', 'should have limiting distance of 1.2');
+  });
+});
