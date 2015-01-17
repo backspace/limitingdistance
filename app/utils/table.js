681e163bb68ed5a81fcbe28ae8027a0aae32022e
Buck Doyle <b@chromatin.ca>
2015-01-17 17:11:34 -0500

    Move still-used prototype extensions near usage

--- app/utils/table.js
+++ app/utils/table.js
@@ -1,3 +1,43 @@
+Array.prototype.uniq = function() {
+    var arr = [];
+    for(var i = 0; i < this.length; i++) {
+        if(arr.indexOf(this[i]) === -1) {
+            arr.push(this[i]);
+        }
+    }
+    return arr;
+};
+
+Array.prototype.first = function(){
+    return this[0];
+};
+
+Array.prototype.last = function(){
+    return this[this.length - 1];
+};
+
+Array.prototype.max = function() {
+  return Math.max.apply(null, this);
+};
+
+Array.prototype.min = function() {
+  return Math.min.apply(null, this);
+};
+
+Array.prototype.clone = function() {
+	return this.slice(0);
+};
+
+Array.prototype.include = function(obj) {
+  var i = this.length;
+  while (i--) {
+      if (this[i] === obj) {
+          return true;
+      }
+  }
+  return false;
+};
+
 var Table = function(lds) {
   this.areaToPercents = {};
   this.lds = lds;
