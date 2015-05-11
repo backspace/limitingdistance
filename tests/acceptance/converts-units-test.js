fdc249b86113571e637908667eccf3a5c6053a47
Buck Doyle <b@chromatin.ca>
2015-05-11 18:13:02 -0400

    Add rounding to calculated imperial area

--- tests/acceptance/converts-units-test.js
+++ tests/acceptance/converts-units-test.js
@@ -37,3 +37,17 @@ test('converts imperial to metric', function() {
     equal(metricHeight, '3048', 'should equal 3048 metres');
   });
 });
+
+test('rounds imperial areas', function() {
+  visit('/');
+
+  click('label:contains(Imperial) input');
+  fillIn('.face-1 input[name=height]', 7);
+  fillIn('.face-1 input[name=width]', 10);
+
+  andThen(function() {
+    var imperialArea = find('.face-1 .calculated-area input').val();
+    equal(imperialArea, '70', 'should equal 70 feet squared');
+  });
+});
+
